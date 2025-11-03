a2211e9a08821924e35b852959c8b9786f6df821
Ivankirev39 <ivakir01@easv365.dk>
2025-11-03 19:41:10 +0100

    Running tests 6  attempt

--- test/todo-app.js
+++ test/todo-app.js
@@ -5,15 +5,25 @@ fixture("ToDo App Tests")
 
 test("Add a new todo item", async t => {
     await t
+        // Arrange + Act
         .typeText(Selector("#todo-input"), "Clean your room")
-        .click(Selector("#submit"))
-        .expect(Selector(".todo-item").withText("Clean your room").exists).ok();
+        .click(Selector(".todo-form button[type='submit']"))
+        .typeText(Selector("#todo-input"), "Wash the dishes")
+        .click(Selector(".todo-form button[type='submit']"))
+        // Assert
+        .expect(Selector("#todo-list").childElementCount).eql(2);
 });
 
 test("Remove a todo item", async t => {
     await t
+        // Arrange + Act
         .typeText(Selector("#todo-input"), "Clean your room")
-        .click(Selector("#submit"))
+        .click(Selector(".todo-form button[type='submit']"))
+        .typeText(Selector("#todo-input"), "Wash the dishes")
+        .click(Selector(".todo-form button[type='submit']"))
+        // Remove one item
         .click(Selector(".todo-item").withText("Clean your room").find("button"))
+        // Assert
+        .expect(Selector("#todo-list").childElementCount).eql(1)
         .expect(Selector(".todo-item").withText("Clean your room").exists).notOk();
 });
