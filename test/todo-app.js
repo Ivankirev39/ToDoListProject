5996db1e763390e06d85a728aa289602b85dc23b
Ivankirev39 <ivakir01@easv365.dk>
2025-11-04 09:33:48 +0100

    fix: Move to test-domain

--- test/todo-app.js
+++ test/todo-app.js
@@ -1,27 +1,25 @@
 import { Selector } from 'testcafe';
 
 fixture("ToDo App Tests")
-    .page("https://ivankirev.dk/test/todo/");
+    .page("https://todo.ivankirev.dk/test/");
 
 test("Add a new todo item", async t => {
-    const input = Selector("#todo-input");
     await t
-        .expect(input.exists).ok({ timeout: 5000 }) // Wait up to 5s for input
-        .typeText(input, "Clean your room")
+        // Arrange + Act
+        .typeText(Selector("#todo-input"), "Clean your room")
         .click(Selector(".todo-form button[type='submit']"))
-        .typeText(input, "Wash the dishes")
+        .typeText(Selector("#todo-input"), "Wash the dishes")
         .click(Selector(".todo-form button[type='submit']"))
+        // Assert
         .expect(Selector("#todo-list").childElementCount).eql(2);
 });
 
 test("Remove a todo item", async t => {
-    const input = Selector("#todo-input");
     await t
-        .expect(input.exists).ok({ timeout: 5000 }) // Wait up to 5s for input
         // Arrange + Act
-        .typeText(input, "Clean your room")
+        .typeText(Selector("#todo-input"), "Clean your room")
         .click(Selector(".todo-form button[type='submit']"))
-        .typeText(input, "Wash the dishes")
+        .typeText(Selector("#todo-input"), "Wash the dishes")
         .click(Selector(".todo-form button[type='submit']"))
         // Remove one item
         .click(Selector(".todo-item").withText("Clean your room").find("button"))
